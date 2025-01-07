82142020e83255774d91f48a064a1efb2183aba6
Cezar Augusto Meira Carmo <cezaraugustomeiracarmo@gmail.com>
2025-01-07 16:24:06 -0300

    Atualização da documentação do projeto

--- data_transformation/transform_sales_data.py
+++ data_transformation/transform_sales_data.py
@@ -1,17 +1,18 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, round, expr
-
+import os  # Import necessário para manipular diretórios
 import findspark
+
 findspark.init("C:/spark")  # Substitua pelo caminho onde você instalou o Spark
 
 # Inicializar SparkSession
 spark = SparkSession.builder \
     .appName("Transformação de Dados de Vendas") \
-    .config("spark.hadoop.native.lib", "false")\
+    .config("spark.hadoop.native.lib", "false") \
     .config("spark.sql.parquet.output.committer.class", "org.apache.parquet.hadoop.ParquetOutputCommitter") \
     .config("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", "2") \
     .config("spark.hadoop.mapreduce.fileoutputcommitter.marksuccessfuljobs", "false") \
-    .config("spark.sql.sources.commitProtocolClass", "org.apache.spark.internal.io.DummyWriteTaskCommitProtocol")\
+    .config("spark.sql.sources.commitProtocolClass", "org.apache.spark.internal.io.DummyWriteTaskCommitProtocol") \
     .getOrCreate()
 
 # Caminho do arquivo de entrada
@@ -35,10 +36,17 @@ sales_df = sales_df.withColumn(
 print("Dados transformados:")
 sales_df.show()
 
+# Caminho do arquivo de saída
+output_dir = "transformed_data"
+output_path = os.path.join(output_dir, "transformed_sales_data.csv")
+
+# Criar o diretório de saída, se não existir
+os.makedirs(output_dir, exist_ok=True)
+
 # Converter para Pandas e salvar
 sales_df_pandas = sales_df.toPandas()
-output_path = "C:/output/transformed_sales_data.csv"
 sales_df_pandas.to_csv(output_path, index=False)
+
 print(f"Dados salvos em {output_path}")
 
 # Encerrar a SparkSession
